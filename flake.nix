42d0419994306bf5df1e7ebaf10f6c0a8e9097fb
Chris Martin <ch.martin@gmail.com>
2025-01-20 17:13:45 -0700

    ghc 9.6, 9.8

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    "nixos-23.05".url = "github:NixOS/nixpkgs/nixos-23.05";
+    "nixos-24.11".url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = inputs@{ self, ... }:
@@ -8,9 +8,9 @@
     in inputs.flake-utils.lib.eachDefaultSystem (system:
       let
         nixpkgs = {
-          "nixos-23.05" = import inputs."nixos-23.05" { inherit system; };
+          "nixos-24.11" = import inputs."nixos-24.11" { inherit system; };
         };
-        pkgs = nixpkgs."nixos-23.05";
+        pkgs = nixpkgs."nixos-24.11";
         project = pkgs.haskellPackages.developPackage {
           root = ./systemd-socket-activation;
           name = packageName;
@@ -23,43 +23,37 @@
       in {
         defaultPackage = self.packages.${system}.${packageName};
 
-        packages = {
+        packages = let
+
+          inherit (pkgs.haskell.lib) dontCheck;
+
+          makeTestConfiguration = let defaultPkgs = pkgs;
+          in { pkgs ? defaultPkgs, ghcVersion, overrides ? new: old: { } }:
+          let inherit (pkgs.haskell.lib) dontCheck packageSourceOverrides;
+          in (pkgs.haskell.packages.${ghcVersion}.override (old: {
+            overrides = combineOverrides old [
+              (packageSourceOverrides {
+                systemd-socket-activation = ./systemd-socket-activation;
+              })
+              overrides
+            ];
+
+          })).systemd-socket-activation;
+
+        in rec {
           "${packageName}" = project;
 
-          testConfigurations = let
-
-            inherit (pkgs.haskell.lib) dontCheck;
-
-            makeTestConfiguration = let defaultPkgs = pkgs;
-            in { pkgs ? defaultPkgs, ghcVersion, overrides ? new: old: { } }:
-            let inherit (pkgs.haskell.lib) dontCheck packageSourceOverrides;
-            in (pkgs.haskell.packages.${ghcVersion}.override (old: {
-              overrides = combineOverrides old [
-                (packageSourceOverrides {
-                  systemd-socket-activation = ./systemd-socket-activation;
-                })
-                overrides
-              ];
-
-            })).systemd-socket-activation;
-
-          in rec {
-            ghc-9-2 = makeTestConfiguration {
-              pkgs = nixpkgs."nixos-23.05";
-              ghcVersion = "ghc92";
-            };
-            ghc-9-4 = makeTestConfiguration {
-              pkgs = nixpkgs."nixos-23.05";
-              ghcVersion = "ghc94";
-            };
-            ghc-9-6 = makeTestConfiguration {
-              pkgs = nixpkgs."nixos-23.05";
-              ghcVersion = "ghc96";
-            };
-            all = pkgs.symlinkJoin {
-              name = packageName;
-              paths = [ ghc-9-2 ghc-9-4 ghc-9-6 ];
-            };
+          ghc-9-6 = makeTestConfiguration {
+            pkgs = nixpkgs."nixos-24.11";
+            ghcVersion = "ghc96";
+          };
+          ghc-9-8 = makeTestConfiguration {
+            pkgs = nixpkgs."nixos-24.11";
+            ghcVersion = "ghc98";
+          };
+          all = pkgs.symlinkJoin {
+            name = packageName;
+            paths = [ ghc-9-6 ghc-9-8 ];
           };
         };
       });
